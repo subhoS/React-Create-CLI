b0ee99ab9a32a07894c71fab6e94d5e0553f28a2
Subhadeep Datta <subhadeepdatta2@gmail.com>
2022-07-08 03:31:13 +0530

    mm changes

--- make_compo.sh
+++ make_compo.sh
@@ -1,3 +1,14 @@
-mkdir -p src/container/$1
-touch src/container/$1/$1.page.tsx
-touch src/container/$1/$1.page.style.ts
+echo "Enter Container Name"
+read VAR1
+
+len=$(echo $VAR1 | awk '{print length($0)}')
+
+if [[ $len -gt 0 ]]
+then
+   mkdir -p src/container/$VAR1
+   touch src/container/$VAR1/$VAR1.page.tsx
+   touch src/container/$VAR1/$VAR1.page.style.ts
+   echo "$VAR1 Container Added Sucessfully"
+else
+   echo "Not a valid container name"
+fi
